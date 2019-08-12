27f8a1b1f4e653720fb84378d345814916a52196
Manoh4 <emanuel.estevao.479@gmail.com>
2019-08-12 06:39:47 -0300

    Fc

---
+++ src/test/Teste1.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Teste1 {
+	
+	@Test
+	public void teste1() {
+			int i = 0;
+			assertTrue("Erro", i==1);
+	}
+
+}

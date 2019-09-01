e24ad1da9e197f0bf79e14570a6b63dc44cb54fb
Manoh4 <emanuel.estevao.479@gmail.com>
2019-09-01 20:53:49 -0300

    Adição de novos testes

--- src/test/Teste1.java
+++ src/test/Teste1.java
@@ -13,21 +13,21 @@ public class Teste1 {
 	}
 
 	@Test
-	public void teste1() {
+	public void teste2() {
 			int i = 0;
 			assertTrue("Erro 2 ", i==1);
 	}
 
 	@Test
-	public void teste1() {
+	public void teste3() {
 			int i = 0;
 			assertTrue("Erro 3", i==0);
 	}
 
 	@Test
-	public void teste1() {
+	public void teste4() {
 			int i = 0;
-			assertTrue("Erro", i==1);
+			assertTrue("Erro 4", i==1);
 	}
 
 }
